388a45e5d46f0f65ea6f69a458f436ebd15c875b
codehimanshu <himanshuagrawal1998@gmail.com>
2020-01-28 23:36:42 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module get-video-duration
+
+go 1.13
+
+require github.com/vansante/go-ffprobe v1.1.0
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	ffprobe "github.com/vansante/go-ffprobe"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	url := r.FormValue("url")
+	if url == "" {
+		w.WriteHeader(400)
+		w.Write([]byte("Url Missing"))
+	}
+	data, err := ffprobe.GetProbeData(url, 10000*time.Millisecond)
+	if err != nil {
+		log.Printf("Error getting data: %v", err)
+		w.WriteHeader(400)
+		w.Write([]byte("Some Error Occurred"))
+	}
+
+	duration := fmt.Sprintf("%f", data.Format.Duration().Seconds())
+	w.WriteHeader(200)
+	w.Write([]byte("{duration:" + duration + "}"))
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
